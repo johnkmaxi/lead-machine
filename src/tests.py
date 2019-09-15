d6f19268f125de89ceabfb8357248f8b87b2103c
johnkmaxi <johnkmaxi@gmail.com>
2019-09-14 21:45:12 -0500

    Add test_insert_data

--- src/tests.py
+++ src/tests.py
@@ -4,11 +4,14 @@
 
 import unittest
 
+import datetime
+
 from scraper import BaseCrawler
 from create_tables import create_database
 from create_tables import delete_database
 from create_tables import create_tables
 from create_tables import drop_tables
+from sql_queries import leads_insert
 
 class TestScraper(unittest.TestCase):
     """
@@ -48,16 +51,25 @@ class TestDb(unittest.TestCase):
         drop_tables(cls.conn)
         cls.conn.close()
 
+    def setUp(self):
+        self.cur = self.conn.cursor()
+
+    def tearDown(self):
+        pass
+
     def test_create_tables(self):
         create_tables(self.conn)
-        cur = self.conn.cursor()
-        cur.execute('select * from leads')
-        results = cur.fetchall()
+        self.cur.execute('select * from leads')
+        results = self.cur.fetchall()
         # results is an empty list
         self.assertEqual(0, len(results))
 
     def test_insert_data(self):
-        pass
+        self.cur = self.conn.cursor()
+        self.cur.execute(leads_insert, [datetime.datetime(2019,1,1),'varchar',1,'varchar',1,1,1,1,'varchar',1,1])
+        self.cur.execute('select * from leads')
+        results = self.cur.fetchall()
+        self.assertEqual(1, len(results))
 
     # def test_insert_duplicate_data(self):
     #     pass
