75576588781b504cd59eadec331e3aae35ed239d
johnkmaxi <johnkmaxi@gmail.com>
2019-09-22 21:47:55 -0500

    Use USERPROFILE in pathname

--- scraper.bat
+++ scraper.bat
@@ -1,6 +1,6 @@
 echo off
 
-CALL  "C:\Users\John Maxi\Anaconda3\Scripts\activate.bat" lema
-python src/main.py
+CALL  "%USERPROFILE%\Anaconda3\Scripts\activate.bat" lema
+python "%USERPROFILE%\lead-machine\src\main.py"
 cmd \k
 echo on 
